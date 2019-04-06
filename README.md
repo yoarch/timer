385043674b72435d208457109153b30ac9b8e9a7
yo <yo.managements@gmail.com>
2019-04-06 22:27:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# timer
+a simple timer/minutor for terminal
+
+a very simple terminal timer in minutes that will play a song and display a message when the time is over.
+
+# installation
+```sh
+with yay:
+yay -a timer
+
+with yaourt:
+yaourt -a timer
+```
+
+# compatibility
+bash
+
+# usage
+<pre>
+<b>timer</b> [TIME_MNS] [AUDIO_PATH] [DISPLAY_MODE]
+</pre>
+
+
+# examples
+for help:<br/>
+```sh
+timer 30
+or
+timer 50 ~/audio/duck_song.mp3
+or
+timer 50 ~/audio/duck_song.mp3 hide
+```
---
+++ timer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# RED='\033[0;31m'
+BWHITE='\033[1m' # bold white
+NC='\033[0m' # No Color
+
+totaltime=${1:-60}
+
+# time display mode can be: "hide" or "display"
+mode=${3:-display}
+
+# echo -e "\nstarting minutor\nducking in $totaltime minutes ...\n"
+printf "\n${BWHITE}starting minutor\nducking in $totaltime minutes ...\n"
+
+for (( c=1; c<=$totaltime; c++ )) do
+	sleep 60
+	time=$c
+	if [[ $mode == "display" ]] && [[ $(($time % 5)) == 0]]; then
+		# echo -e "\e[1m$time minutes"
+		printf "$time minutes"
+	fi
+done
+
+
+printf "I ${BWHITE}love${NC} Stack Overflow\n"
+
+echo -e "end of the time\n"
+# echo -e "\e[1mend of the time\n"
+
+mpv ${2:-$MHOME/dev/inputs/utopia_alarm.mp3 > /dev/null 2>&1 &}
+
+echo "\e[1mmpv pid: $$${NC}"
