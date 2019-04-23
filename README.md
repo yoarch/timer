56a251166e774ded8038c179d417547a5876441f
yo <yo.managements@gmail.com>
2019-04-23 16:29:30 +0200

    adding hours, minutes and seconds

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # timer
-a simple timer/minutor for terminal
+a simple timer for terminal
 
-a very simple terminal timer in minute that will play a song and display a message when time is over.
+a very simple terminal timer in second, minute or hour, that will play a song and display a message when time is over.
 
 # installation
 ```sh
@@ -13,16 +13,27 @@ yaourt -a timer
 
 # usage
 <pre>
-<b>timer</b> [TIME_MNS] [AUDIO_PATH] [DISPLAY_MODE]
+<b>timer</b> [TOTAL_TIME] [TIME_MODE] [AUDIO_PATH] [DISPLAY_MODE]
+<b>options:</b>
+<!-- -->         <b>TOTAL_TIME</b>        must be an integer such as 42
+<!-- -->         <b>TIME_MODE</b>        "h" for hours, "m" for minutes or "s" for seconds
+<!-- -->         <b>AUDIO_PATH</b>        to play another audio than the duck one by default
+<!-- -->         <b>DISPLAY_MODE</b>        "hide" or "display": display every minute spent with a dot, enable by default
+<!-- -->         <b>-h, --help</b>        show this help message and exit
 </pre>
 
 
 # examples
 for help:<br/>
+time in 30 seconds
 ```sh
-timer 30
-or
-timer 50 ~/audio/duck_song.mp3
-or
-timer 50 ~/audio/duck_song.mp3 hide
+timer 30 s
+```
+time in 50 minutes
+```sh
+timer 50 m ~/audio/duck_song.mp3
+```
+time in 2 hours
+```sh
+timer 2 h ~/audio/duck_song.mp3 hide
 ```
--- timer.sh
+++ timer.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
 function check_args {
-	if [ -z $1 ]; then
-		echo -e "\n\t${BWHITE}needs at least one argument being the time in minute you want the timer to stop in\n\tfor 2 minutes enter:\n\ttimer 2"
-		exit 1
-	fi
 
 	if [ "$#" -eq 1 ] && ([[ $1 == "-h" ]] || [[ $1 == "--help" ]]); then
     cat /usr/lib/timer/README.md
 		exit 0
 	fi
 
+	if [ -z $2 ]; then
+		echo -e "\n\t${BWHITE}needs at least two arguments being the time in timemode\n\tfor 2 minutes enter:\n\ttimer 2 m"
+		exit 1
+	fi
+
 	if ! [[ "$1" =~ ^[0-9]+$ ]]; then
 		echo -e "\n\t${BWHITE}first argument must be an integer being the time in minute you want the timer to stop in\n\tfor 2 minutes enter:\n\ttimer 2"
 		exit 1
+	fi
+}
+
+function args_to_scds {
+	x_seconds=0
+	if [[ $2 == "h" ]]; then
+		x_seconds=$(($1*3600))
+	elif [[ $2 == "m" ]]; then
+		x_seconds=$(($1*60))
+	elif [[ $2 == "s" ]]; then
+		x_seconds=$1
+	else
+		echo -e "$2 arg is not supported, please enter \"h\", \"m\" or \"s\""
+		return 1
 	fi
 }
 
@@ -23,21 +38,30 @@ NC='\033[0m' # no color
 check_args "$@"
 
 totaltime=$1
+# timemode can be "h": hours, "m": minutes, "s": seconds
+timemode=$2
 
 # time display mode can be: "hide" or "display"
-mode=${3:-display}
+mode=${4:-display}
 
-echo -e "\n\t${BWHITE}starting timer ($(date +"%T"))\n\n\tducking in $totaltime minutes ...\n\n"
+args_to_scds $totaltime $timemode
 
-for (( time=1; time<=$totaltime; time++ )) do
-	sleep 60
-	if [[ $mode == "display" ]] && [[ $(($time % 5)) == 0 ]]; then
-		echo -e "\t\t$time minutes"
+echo -e "\n\t${BWHITE}starting timer ($(date +"%T"))\n\n\tducking in $totaltime$timemode ...\n\n"
+
+i=0
+for (( time_s=1; time_s<=$x_seconds; time_s++ )) do
+	sleep 1
+	if [[ $mode == "display" ]] && [[ $(($time_s % 60)) == 0 ]]; then
+		i=$((i+1))
+		printf "\n$i: "
+		for (( d=1; d<=$i; d++ )) do
+			printf "."
+		done
 	fi
 done
 echo -e "\n\tend of the time\n"
 
-audio_path=${2:-"/usr/share/sounds/timer/duck.wav"}
+audio_path=${3:-"/usr/share/sounds/timer/duck.wav"}
 mpv $audio_path > /dev/null 2>&1 &
 
 echo -e "mpv pid: $$${NC}\n"
